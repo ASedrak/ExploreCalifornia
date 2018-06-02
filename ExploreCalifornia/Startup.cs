b51982076d0a2d31b5c5d2e2ea443f6803b26872
LEX <LEX @DESKTOP-2T2KUKP>
2018-06-01 21:57:49 -0400

    include "invalid" Error page

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace ExploreCalifornia
 {
@@ -16,10 +17,32 @@ namespace ExploreCalifornia
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            // runs an error page
+            app.UseExceptionHandler("/error.html");
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+
+            // tells it to use the error page, 
+            // To get it to work go to project -> properties -> Debug -> change the value for "ASPNETCORE_ENVIRONMENT"
+            app.Use(async (context, next) =>
+            {
+
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("Error!");
+
+                await next();
+
+
+            });
+
+            
 
 
             app.UseFileServer();
