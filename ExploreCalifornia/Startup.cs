0d2bf7307ccb672e1b608f902143db502a40c36a
LEX <LEX @DESKTOP-2T2KUKP>
2018-06-01 22:18:42 -0400

    debug error config file setup... must remove before development for security issues

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -8,6 +9,8 @@ namespace ExploreCalifornia
 {
     public class Startup
     {
+        private readonly object loggerFactory;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -18,11 +21,26 @@ namespace ExploreCalifornia
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
 
+
+           
+
+
+
             // runs an error page
             app.UseExceptionHandler("/error.html");
 
 
-            if (env.IsDevelopment())
+            // remove before deployment for security purposes 
+            var configuration = new ConfigurationBuilder()
+                                    .AddEnvironmentVariables()
+                                    .AddJsonFile(env.ContentRootPath + "/config.json")
+                                    .Build();
+            // used to add debug error page in testing phase.                                      
+                                    
+
+
+
+            if (configuration.GetValue<bool>("EnableDeveloperException"))
             {
                 app.UseDeveloperExceptionPage();
             }
